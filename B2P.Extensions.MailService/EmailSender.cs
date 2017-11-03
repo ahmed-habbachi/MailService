61c8935570f570c35d72df1d83640c924f05a379
Ahmed Habbachi <ahmed.habbachi@outlook.com>
2017-11-03 16:43:34 +0100

    Add project files.

---
+++ B2P.Extensions.MailService/EmailSender.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace B2P.Extensions.MailService
+{
+    public class EmailSender : IEmailSender
+    {
+        public AuthMessageSenderOptions Options { get; }
+
+        public EmailSender(AuthMessageSenderOptions senderOptions)
+        {
+            Options = senderOptions;
+        }
+
+        public Task SendEmailAsync(string toEmail, string subject, string message)
+        {
+            MailMessage mail = GetMailMessage(toEmail, subject, message,
+                Options.DefaultSenderEmail, Options.DefaultSenderDisplayName, Options.UseHtml);
+            SmtpClient client = GetSmtpClient(Options.Domain, Options.Port, Options.RequiresAuthentication,
+                Options.UserName, Options.Key, Options.UseSsl);
+
+            return client.SendMailAsync(mail);
+        }
+
+        public void SendEmail(string toEmail, string subject, string message)
+        {
+            MailMessage mail = GetMailMessage(toEmail, subject, message,
+                Options.DefaultSenderEmail, Options.DefaultSenderDisplayName, Options.UseHtml);
+            SmtpClient client = GetSmtpClient(Options.Domain, Options.Port, Options.RequiresAuthentication,
+                Options.UserName, Options.Key, Options.UseSsl);
+
+            client.Send(mail);
+        }
+
+        private static MailMessage GetMailMessage(string toEmail, string subject, string message,
+            string defaultSenderEmail, string defaultSenderDisplayName = null, bool useHtml = true)
+        {
+            MailAddress sender;
+
+            if (string.IsNullOrEmpty(defaultSenderEmail))
+            {
+                throw new ArgumentException("No sender mail address was provided");
+            }
+            else
+            {
+                sender = !string.IsNullOrEmpty(defaultSenderDisplayName) ?
+                    new MailAddress(defaultSenderEmail, defaultSenderDisplayName) : new MailAddress(defaultSenderEmail);
+            }
+
+            MailMessage mail = new MailMessage()
+            {
+                From = sender,
+                Subject = subject,
+                Body = message,
+                IsBodyHtml = useHtml
+            };
+            mail.To.Add(toEmail);
+            return mail;
+        }
+
+        private static SmtpClient GetSmtpClient(string host, int port, bool requiresAuthentication = true,
+            string userName = null, string userKey = null, bool useSsl = false)
+        {
+            SmtpClient client = new SmtpClient();
+
+            if (string.IsNullOrEmpty(host))
+            {
+                throw new ArgumentException("No domain was provided");
+            }
+
+            client.Host = host;
+
+            if (port > -1)
+            {
+                client.Port = port;
+            }
+
+            client.UseDefaultCredentials = !requiresAuthentication;
+
+            if (requiresAuthentication)
+            {
+                if (string.IsNullOrEmpty(userName))
+                {
+                    throw new ArgumentException("No user name was provided");
+                }
+
+                client.Credentials = new NetworkCredential(userName, userKey);
+            }
+
+            client.EnableSsl = useSsl;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            return client;
+        }
+    }
+}
---
+++ B2P.Extensions.MailService/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace B2P.Extensions.MailService
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+        void SendEmail(string email, string subject, string message);
+    }
+}
